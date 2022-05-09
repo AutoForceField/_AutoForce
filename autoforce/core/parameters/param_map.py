4c630f9a78b0a62f45b12d71f02576295a65a2d7
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2022-05-09 15:27:10 +0900

    module: proxy_map

--- autoforce/core/parameters/param_map.py
+++ autoforce/core/parameters/param_map.py
@@ -1,88 +1,27 @@
 # +
-from collections import defaultdict
-from itertools import product, repeat
-from typing import Iterable, Iterator, Mapping, Optional, Sized, Union
+from typing import Optional, Union
 
-__all__ = ["ParamMap", "SymParamMap"]
+from .proxy_map import DefaultProxyMap, SortedKey, TupleKey
+
+__all__ = ["ParamMap", "SymmetricParamMap"]
 
 
 # typing aliases:
-SpeciesType = int
 ParamType = float
-KeyType = Union[SpeciesType, tuple[SpeciesType, ...]]
+KeyType = Union[int, tuple[int, ...]]
 MappingType = dict[KeyType, ParamType]
-TupleKeyType = tuple[SpeciesType, ...]
-TupleMappingType = dict[TupleKeyType, ParamType]
 
 
-class ParamMap(Mapping):
+class ParamMap(TupleKey, DefaultProxyMap):
     def __init__(
         self,
         default: ParamType,
         defined: Optional[MappingType] = None,
-        indices: Optional[int] = None,
     ) -> None:
         if defined is None:
             defined = {}
-        if indices is None:
-            try:
-                (indices,) = {len(_tuple(k)) for k in defined}
-            except ValueError:
-                raise RuntimeError("Num indices is not unique or can't be inferred!")
-
-        def callable_() -> ParamType:
-            return default
-
-        self.indices: int = indices
-        dict_: TupleMappingType = {self._key(k): v for k, v in defined.items()}
-        self.dict = defaultdict(callable_, dict_)
-
-    # public methods:
-    def broadcast(
-        self, *args: Union[SpeciesType, Iterable[SpeciesType]]
-    ) -> tuple[ParamType, ...]:
-        assert len(args) == self.indices, f"Num args should be {self.indices}!"
-        assert any(
-            tuple(isinstance(arg, Sized) for arg in args)
-        ), "At least one of args should be Sized!"
-        bcast = tuple(arg if isinstance(arg, Iterable) else repeat(arg) for arg in args)
-        ret = tuple(self.dict[self._key(k)] for k in zip(*bcast))
-        # timings: ~ 100 us
-        return ret
-
-    def product(self, species: set[SpeciesType]) -> MappingType:
-        return {
-            _int(self._key(k)): self[k] for k in product(species, repeat=self.indices)
-        }
-
-    # Mapping methods:
-    def __getitem__(self, k: KeyType) -> ParamType:
-        return self.dict[self._key(k)]
-
-    def __len__(self) -> int:
-        return len(self.dict)
-
-    def __iter__(self) -> Iterator[TupleKeyType]:
-        return iter(self.dict)
-
-    # private methods:
-    def _key(self, k: KeyType) -> TupleKeyType:
-        key = (k,) if isinstance(k, int) else k
-        # TODO: remove assert
-        assert len(key) == self.indices, f"Length of key should be {self.indices}!"
-        return key
+        super().__init__(default, defined)
 
 
-class SymParamMap(ParamMap):
-
-    # override:
-    def _key(self, k: KeyType) -> TupleKeyType:
-        return tuple(sorted(super()._key(k)))
-
-
-def _tuple(k: KeyType) -> TupleKeyType:
-    return (k,) if isinstance(k, int) else k
-
-
-def _int(k: TupleKeyType) -> KeyType:
-    return k if len(k) > 1 else k[0]
+class SymmetricParamMap(SortedKey, ParamMap):
+    pass
---
+++ autoforce/core/parameters/proxy_map.py
@@ -0,0 +1,73 @@
+# +
+from abc import abstractmethod
+from collections import defaultdict
+from collections.abc import MutableMapping
+from itertools import product, repeat
+from typing import Any, Hashable, Iterable, Iterator, Sized, Union
+
+
+class ProxyMap(MutableMapping):
+    def __init__(self, dict_: Union[dict, defaultdict]) -> None:
+        self._dict = dict_.copy()
+        self._dict.clear()
+        for k, v in dict_.items():
+            self._dict[self._key(k)] = v
+
+    @abstractmethod
+    def _key(self, key: Any) -> Hashable:
+        ...
+
+    def __getitem__(self, key: Any) -> Any:
+        return self._dict[self._key(key)]
+
+    def __setitem__(self, key: Any, value: Any) -> None:
+        self._dict[self._key(key)] = value
+
+    def __delitem__(self, key: Any) -> None:
+        del self._dict[self._key(key)]
+
+    def __len__(self) -> int:
+        return len(self._dict)
+
+    def __iter__(self) -> Iterator:
+        return iter(self._dict)
+
+    def __repr__(self) -> str:
+        return repr(self._dict)
+
+    def broadcast(self, args: Iterable) -> tuple[Any, ...]:
+        """
+        args = (1, (1, 2)) --> values for (1, 1), (1, 2)
+        """
+        # For avoiding infinite loops:
+        sized = tuple(isinstance(arg, Sized) for arg in args)
+        assert any(sized), "At least one of args should be Sized!"
+        #
+        bcast = tuple(arg if isinstance(arg, Iterable) else repeat(arg) for arg in args)
+        return tuple(self[key] for key in zip(*bcast))
+
+    def product(self, args: Iterable, repeat: int = 1) -> dict:
+        """
+        args = (1, 2), repeat = 2 --> a dict from keys (1, 1), (1, 2), (2, 1), (2, 2)
+        """
+        keys = (self._key(k) for k in product(args, repeat=repeat))
+        return {key: self._dict[key] for key in keys}
+
+
+class DefaultProxyMap(ProxyMap):
+    def __init__(self, default: Any, dict_: dict) -> None:
+        def callable_():
+            return default
+
+        _dict = defaultdict(callable_, dict_)
+        super().__init__(_dict)
+
+
+class TupleKey:
+    def _key(self, key: Any) -> tuple:
+        return key if isinstance(key, tuple) else (key,)
+
+
+class SortedKey:
+    def _key(self, key: Any) -> tuple:
+        return tuple(sorted(key)) if isinstance(key, tuple) else (key,)
