729268ceab053a8b00adada209aee6be64c61f56
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2022-05-11 23:28:55 +0900

    SoftZero_fn

---
+++ autoforce/functions/tests/test_softzeros.py
@@ -0,0 +1,32 @@
+# +
+import torch
+
+from autoforce.functions.softzeros import Polynomial_sz, Sine_sz, SoftZero_fn
+
+
+def _test(f: SoftZero_fn) -> bool:
+    f = Polynomial_sz(2)
+    x = torch.tensor(0.0, requires_grad=True)
+    y = f.function(x)
+    assert y.isclose(x)
+    (g,) = torch.autograd.grad(y, x)
+    assert g.isclose(x)
+    x = torch.tensor(1.0)
+    y = f.function(x)
+    assert y.isclose(x)
+    x = torch.rand(10)
+    y = f.function(x)
+    assert (y > 0.0).all() and (y < 1.0).all()
+    return True
+
+
+def test_Polynomial_sz() -> bool:
+    return _test(Polynomial_sz(2))
+
+
+def test_Sine_sz() -> bool:
+    return _test(Sine_sz())
+
+
+if __name__ == "__main__":
+    test_Polynomial_sz()
