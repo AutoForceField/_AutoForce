8753b722b818c1dbcbf36bf13f9037245467c2ec
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2022-05-11 15:43:09 +0900

    requirements -> setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 # +
 from setuptools import find_packages, setup
 
+with open("autoforce/version.py") as f:
+    version: dict[str, str] = {}
+    exec(f.read(), version)
+__version__ = version["__version__"]
+
+
 setup(
     name="autoforce",
-    version="0",
+    version=__version__,
     author="Amir Hajibabaei",
     author_email="a.hajibabaei.86@gmail.com",
     description="machine learning force-fields",
-    url="https://github.com",
     packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=["numpy", "scipy", "torch>=1.10", "ase"],
+    url="https://github.com",
+    license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
