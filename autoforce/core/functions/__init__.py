9bf6ecfca3b343d3fe254156f699ff2eaf66546b
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2022-05-12 13:02:54 +0900

    removed Cutoff_fn

--- autoforce/core/functions/__init__.py
+++ autoforce/core/functions/__init__.py
@@ -1,6 +1,5 @@
 # +
 from .bijection_fn import Bijection_fn
-from .cutoff_fn import Cutoff_fn
 from .descriptor_fn import Descriptor_fn
 from .kernel_fn import Kernel_fn
 from .map_fn import Map_fn
@@ -8,7 +7,6 @@ from .softzero_fn import SoftZero_fn
 
 __all__ = [
     "Bijection_fn",
-    "Cutoff_fn",
     "Map_fn",
     "Descriptor_fn",
     "Kernel_fn",
